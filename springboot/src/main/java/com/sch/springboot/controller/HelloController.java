da91953f561fafdd1e241da06a96f7fc8fd8fa4d
Heo Juwan <eric5519@naver.com>
2025-08-25 17:07:08 +0900

    2025-08-25 commit

---
+++ springboot/src/main/java/com/sch/springboot/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.sch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/spring-mvc")
+    public String sprigMvc(Model model){
+        model.addAttribute("msg","HIHIHI");
+        return "spring-mvc";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        //hello.html view에게 데이터 전달  --> Model
+        model.addAttribute("msg", "홍길동");
+        return "hello";     // view name을 리턴    msg = "홍길동"
+    }
+
+
+}
