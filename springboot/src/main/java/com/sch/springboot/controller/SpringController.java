9511dafd03b025e59c9464c8957b1246dbe9cfcd
Heo Juwan <eric5519@naver.com>
2025-08-26 09:30:35 +0900

    2025-08-25 commit

---
+++ springboot/src/main/java/com/sch/springboot/controller/SpringController.java
@@ -0,0 +1,12 @@
+package com.sch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpringController {
+    @GetMapping("/spring")
+    public String spring(){
+        return "spring";    // view name
+    }
+}
